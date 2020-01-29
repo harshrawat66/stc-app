33428cc4c29fb722884f9d3006aee5dbde489891
Harsh Rawat <harshrawat66@gmail.com>
2020-01-29 17:40:19 +0530

    cluster link addition

--- src/configBreakout.js
+++ src/configBreakout.js
@@ -1,4 +1,4 @@
-const dbPath = 'mongodb://127.0.0.1:27017/stc-app-api' ;
+const dbPath = 'mongodb+srv://stcDevAdmin:<password>@stc-api-zarnj.mongodb.net/test?retryWrites=true&w=majority' ;
 const jwtSigningKey = '8113c6ce3ef2267ddec322d0525a721155501f340abab350a496f6702f7b972c'
 
 module.exports = {
